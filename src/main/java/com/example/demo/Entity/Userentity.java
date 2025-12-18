c4b3ca8522b093036c275108d27208a57371d63e
bala-amypo <bala@amypo.in>
2025-12-18 10:02:43 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Userentity.java
+++ src/main/java/com/example/demo/Entity/Userentity.java
@@ -1,8 +1,15 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String password;
@@ -19,37 +26,42 @@ public class UserEntity {
     }
 
     public Long getId() {
-         return id;
-          }
-    public void setId(Long id) { 
-    this.id = id; 
+        return id;
     }
 
-    public String getName() { 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
         return name;
-         }
+    }
+
     public void setName(String name) {
-         this.name = name;
-          }
+        this.name = name;
+    }
 
-    public String getEmail() { 
-        return email; 
-        }
-    public void setEmail(String email) { 
-        this.email = email; 
-        }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getPassword() { 
-        return password; 
-        }
-    public void setPassword(String password) { 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
-         }
+    }
 
-    public String getRole() { 
-        return role; 
-        }
-    public void setRole(String role) { 
-        this.role = role; 
-        }
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
