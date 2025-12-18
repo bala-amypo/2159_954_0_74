b96889f8f5648fbfbee3fc3c63d6c1e7486808f3
bala-amypo <bala@amypo.in>
2025-12-18 09:02:44 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Userentity.java
+++ src/main/java/com/example/demo/Entity/Userentity.java
@@ -1,19 +1,16 @@
-package com.example.project1.entity;
+package com.example.demo.entity;
 
-public class User {
+public class UserEntity {
 
-    private long id;
+    private Long id;
     private String name;
     private String email;
     private String password;
     private String role;
 
-    // No-argument constructor
-    public User() {
-    }
+    public UserEntity() {}
 
-    // Parameterized constructor
-    public User(long id, String name, String email, String password, String role) {
+    public UserEntity(Long id, String name, String email, String password, String role) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -21,48 +18,38 @@ public class User {
         this.role = role;
     }
 
-    // Getter & Setter for id
-    public long getId() {
-        return id;
+    public Long getId() {
+         return id;
+          }
+    public void setId(Long id) { 
+    this.id = id; 
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    // Getter & Setter for name
-    public String getName() {
+    public String getName() { 
         return name;
-    }
-
+         }
     public void setName(String name) {
-        this.name = name;
-    }
+         this.name = name;
+          }
 
-    // Getter & Setter for email
-    public String getEmail() {
-        return email;
-    }
+    public String getEmail() { 
+        return email; 
+        }
+    public void setEmail(String email) { 
+        this.email = email; 
+        }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    // Getter & Setter for password
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
+    public String getPassword() { 
+        return password; 
+        }
+    public void setPassword(String password) { 
         this.password = password;
-    }
+         }
 
-    // Getter & Setter for role
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+    public String getRole() { 
+        return role; 
+        }
+    public void setRole(String role) { 
+        this.role = role; 
+        }
 }
